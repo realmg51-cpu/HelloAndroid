59a5fcf7973562bf5d8bc59805593646e6bf8c17
RealMG <realmg51@gmail.com>
2026-04-13 20:02:45 +0700

    Add Android and Kotlin plugins to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
-// Top-level build file
+plugins {
+    id("com.android.application") apply false
+    id("org.jetbrains.kotlin.android") apply false
+}
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
